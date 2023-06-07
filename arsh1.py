f27cf128bf4b2ff967866aa6f9d035a0c7900343
Y Name <maniharafreen1408@gmail.com>
2023-06-07 17:56:25 +0530

    new changes

--- arsh1.py
+++ arsh1.py
@@ -14,11 +14,11 @@ MARGIN_RIGHT = 200
 
 # Section dimensions
 SECTION_WIDTH = 520
-SECTION_HEIGHT = (PAGE_HEIGHT - MARGIN_TOP - MARGIN_BOTTOM - 800) // 6
+SECTION_HEIGHT = 392
 
 # Column dimensions
 COLUMN_WIDTH = 520
-COLUMN_HEIGHT = SECTION_HEIGHT
+COLUMN_HEIGHT = 392
 
 # Gap between sections and columns
 SECTION_GAP = 20
@@ -26,52 +26,44 @@ COLUMN_GAP = 20
 
 # Bubble options
 OPTIONS = ["A", "B", "C", "D"]
-BUBBLE_RADIUS = 10
-BUBBLE_GAP = 30  # Gap between two bubbles
-LABEL_GAP = 5
+BUBBLE_RADIUS = 12
+
+# Gap between numbering and label/bubble
 NUMBERING_GAP = 20
+LABEL_BUBBLE_GAP = 10
 
 # Create a new PDF document
 canvas = Canvas("omr_sheet.pdf", pagesize=portrait((PAGE_WIDTH, PAGE_HEIGHT)))
 
-# Draw four sections
+# Draw the four sections below the existing columns
+section_x = MARGIN_LEFT
+section_y = MARGIN_BOTTOM
+
 for i in range(4):
-    # Calculate the position of each section
-    section_x = MARGIN_LEFT + i * (SECTION_WIDTH + SECTION_GAP)
-    section_y = MARGIN_BOTTOM + 100
-
     # Draw the section rectangle
-    canvas.rect(section_x, section_y, SECTION_WIDTH, SECTION_HEIGHT * 6)
-
-    # Add the question labels
-    question_label_x = section_x + 10
-    question_label_y = section_y + SECTION_HEIGHT - 20 - NUMBERING_GAP
-    canvas.setFont("Helvetica", 12)
-    for j in range(5):
-        question_label_number = i * 20 + j + 1
-        canvas.drawString(question_label_x, question_label_y - j * 45 - NUMBERING_GAP, f"{question_label_number}.")
+    canvas.rect(section_x, section_y, SECTION_WIDTH, SECTION_HEIGHT)
 
     # Draw six columns within the section
     for j in range(6):
         # Calculate the position of each column within the section
-        column_x = section_x + j * (COLUMN_WIDTH + COLUMN_GAP)
-        column_y = section_y
+        column_x = section_x
+        column_y = section_y + (5 - j) * (COLUMN_HEIGHT + COLUMN_GAP)
 
         # Draw the column rectangle
         canvas.rect(column_x, column_y, COLUMN_WIDTH, COLUMN_HEIGHT)
 
         # Add the question numbers and options
         question_x = column_x + 10
-        question_y = column_y + COLUMN_HEIGHT - 20 - NUMBERING_GAP
+        question_y = column_y + COLUMN_HEIGHT - 20
 
         for k in range(5):
-            question_number = i * 20 + j * 5 + k + 1
+            question_number = i * 30 + j * 5 + k + 1
             canvas.setFont("Helvetica", 12)
-            canvas.drawString(question_x, question_y - k * 45 - NUMBERING_GAP, f"{question_number}.")
+            canvas.drawString(question_x, question_y - k * 80 - NUMBERING_GAP, f"{question_number}.")
 
             for l, option in enumerate(OPTIONS):
-                bubble_x = question_x + 40 + (l * (2 * BUBBLE_RADIUS + BUBBLE_GAP)) + NUMBERING_GAP
-                bubble_y = question_y - k * 45 - 30
+                bubble_x = question_x + 40 + (l * 100) + NUMBERING_GAP
+                bubble_y = question_y - k * 80 - 30
 
                 # Draw the bubble option circle
                 canvas.setStrokeColor(colors.black)
@@ -82,10 +74,13 @@ for i in range(4):
 
                 # Add the option label
                 canvas.setFillColor(colors.black)
-                canvas.setFont("Helvetica", 10)
+                canvas.setFont("Helvetica", 12)
                 label_x = bubble_center_x
-                label_y = bubble_center_y + BUBBLE_RADIUS + LABEL_GAP
+                label_y = bubble_center_y + BUBBLE_RADIUS + LABEL_BUBBLE_GAP
                 canvas.drawCentredString(label_x, label_y, option)
+
+    # Update the section coordinates for the next section
+    section_x += SECTION_WIDTH + SECTION_GAP
 
 # Save and close the PDF document
 canvas.save()
---
+++ jc.py
@@ -0,0 +1,219 @@
+from reportlab.lib.pagesizes import portrait
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib import colors
+
+# Page dimensions
+PAGE_WIDTH = 2480
+PAGE_HEIGHT = 3508
+
+# Margins
+MARGIN_TOP = 150
+MARGIN_BOTTOM = 200
+MARGIN_LEFT = 200
+MARGIN_RIGHT = 200
+
+
+
+# Section dimensions
+SECTION_WIDTH = 520
+SECTION_HEIGHT = (PAGE_HEIGHT - MARGIN_TOP - MARGIN_BOTTOM - 1150) // 6
+
+
+# Gap between sections and columns
+SECTION_GAP = 20
+COLUMN_GAP = 20
+
+# Bubble options
+OPTIONS = ["A", "B", "C", "D"]
+BUBBLE_RADIUS = 12
+BUBBLE_GAP = 30
+LABEL_GAP = 5
+NUMBERING_GAP = 20
+
+
+
+# Column dimensions
+LEFT_COLUMN_WIDTH = 520
+RIGHT_COLUMN_WIDTH = 1200
+COLUMN_HEIGHT = 550
+
+# Bubble options
+OPTIONS = ["A", "B", "C", "D"]
+BUBBLE_RADIUS = 10
+BUBBLE_GAP = 10
+LABEL_GAP = 10
+NUMBERING_GAP = 20
+
+# Create a new PDF document
+canvas = Canvas("omr_sheet.pdf", pagesize=portrait((PAGE_WIDTH, PAGE_HEIGHT)))
+
+# Draw the left column
+left_column_x = MARGIN_LEFT
+left_column_y = PAGE_HEIGHT - MARGIN_TOP - COLUMN_HEIGHT - 100
+canvas.rect(left_column_x, left_column_y, LEFT_COLUMN_WIDTH, COLUMN_HEIGHT)
+
+# Add the question numbers and options in the left column
+question_x = left_column_x + 10
+question_y = left_column_y + COLUMN_HEIGHT - 20 - NUMBERING_GAP
+
+num_questions = 10
+num_options = 4
+
+
+
+for question in range(num_questions):
+    question_number = question
+
+    canvas.setFont("Helvetica", 15)
+    canvas.drawString(question_x, question_y - question * 45 - NUMBERING_GAP, f"{question_number}.")
+
+    for k in range(num_options):
+        if k >= len(OPTIONS):
+            break
+
+        option = OPTIONS[k]
+        bubble_x = question_x + 40 + (k * 60) + NUMBERING_GAP
+        bubble_y = question_y - question * 45 - 30
+
+        # Draw the bubble option circle
+        canvas.setStrokeColor(colors.black)
+        canvas.setFillColor(colors.white)
+        bubble_center_x = bubble_x + BUBBLE_RADIUS
+        bubble_center_y = bubble_y + BUBBLE_RADIUS
+        canvas.circle(bubble_center_x, bubble_center_y, BUBBLE_RADIUS, fill=1, stroke=1)
+
+        # Add the option label
+        canvas.setFillColor(colors.black)
+        canvas.setFont("Helvetica", 15)
+        label_x = bubble_center_x
+        label_y = bubble_center_y + BUBBLE_RADIUS + LABEL_GAP
+        canvas.drawCentredString(label_x, label_y, option)
+
+# Draw the four small columns above the left column
+small_column_width = 20
+small_column_height = 20
+small_column_x = left_column_x + BUBBLE_RADIUS - small_column_width // 2 + 80 # Add a gap of 40 pixels
+
+for _ in range(4):
+    small_column_y = left_column_y + COLUMN_HEIGHT + BUBBLE_GAP - small_column_height + 40
+    canvas.rect(small_column_x, small_column_y, small_column_width, small_column_height)
+    small_column_x += (2 * BUBBLE_RADIUS) + BUBBLE_GAP + 28  # Add a gap of 40 pixels
+
+# Add the "Roll No:" label on the left side
+roll_label_x = left_column_x - (-10)
+roll_label_y = left_column_y + COLUMN_HEIGHT + 30  # Add a gap of 40 pixels
+canvas.setFont("Helvetica", 15)
+canvas.drawString(roll_label_x, roll_label_y, "Roll No:")
+
+
+
+# Draw the right column
+right_column_x = left_column_x + LEFT_COLUMN_WIDTH + 400
+right_column_y = left_column_y
+canvas.rect(right_column_x, right_column_y, RIGHT_COLUMN_WIDTH, COLUMN_HEIGHT)
+
+# Add labels in the top-right column with increased font size and margin
+label_x = right_column_x + 50
+label_y = right_column_y + COLUMN_HEIGHT - 20 - NUMBERING_GAP
+label_gap = 120
+
+canvas.setFont("Helvetica", 40)  # Change font size to 20px
+canvas.drawString(label_x, label_y - label_gap, "NAME:")
+canvas.drawString(label_x, label_y - 2 * label_gap, "EXAM:")
+canvas.drawString(label_x, label_y - 3 * label_gap, "DATE:")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Section dimensions
+SECTION_WIDTH = 520
+SECTION_HEIGHT = 392
+
+# Column dimensions
+COLUMN_WIDTH = 520
+COLUMN_HEIGHT = 392
+
+# Gap between sections and columns
+SECTION_GAP = 20
+COLUMN_GAP = 20
+
+# Bubble options
+OPTIONS = ["A", "B", "C", "D"]
+BUBBLE_RADIUS = 12
+
+# Gap between numbering and label/bubble
+NUMBERING_GAP = 20
+LABEL_BUBBLE_GAP = 10
+
+
+# Draw the four sections below the existing columns
+section_x = MARGIN_LEFT
+section_y = MARGIN_BOTTOM
+
+for i in range(4):
+    # Draw the section rectangle
+    canvas.rect(section_x, section_y, SECTION_WIDTH, SECTION_HEIGHT)
+
+    # Draw six columns within the section
+    for j in range(6):
+        # Calculate the position of each column within the section
+        column_x = section_x
+        column_y = section_y + (5 - j) * (COLUMN_HEIGHT + COLUMN_GAP)
+
+        # Draw the column rectangle
+        canvas.rect(column_x, column_y, COLUMN_WIDTH, COLUMN_HEIGHT)
+
+        # Add the question numbers and options
+        question_x = column_x + 10
+        question_y = column_y + COLUMN_HEIGHT - 20
+
+        for k in range(5):
+            question_number = i * 30 + j * 5 + k + 1
+            canvas.setFont("Helvetica", 15)
+            canvas.drawString(question_x, question_y - k * 80 - NUMBERING_GAP, f"{question_number}.")
+
+            for l, option in enumerate(OPTIONS):
+                bubble_x = question_x + 40 + (l * 100) + NUMBERING_GAP
+                bubble_y = question_y - k * 80 - 30
+
+                # Draw the bubble option circle
+                canvas.setStrokeColor(colors.black)
+                canvas.setFillColor(colors.white)
+                bubble_center_x = bubble_x + BUBBLE_RADIUS
+                bubble_center_y = bubble_y + BUBBLE_RADIUS
+                canvas.circle(bubble_center_x, bubble_center_y, BUBBLE_RADIUS, fill=1, stroke=1)
+
+                # Add the option label
+                canvas.setFillColor(colors.black)
+                canvas.setFont("Helvetica", 15)
+                label_x = bubble_center_x
+                label_y = bubble_center_y + BUBBLE_RADIUS + LABEL_BUBBLE_GAP
+                canvas.drawCentredString(label_x, label_y, option)
+
+    # Update the section coordinates for the next section
+    section_x += SECTION_WIDTH + SECTION_GAP
+
+# Save and close the PDF document
+canvas.save()
